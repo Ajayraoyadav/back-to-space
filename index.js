5fd3a268d344b15d4197858f556f2a4091a27080
Ajay Yadav <97391719+Ajayraoyadav@users.noreply.github.com>
2024-08-06 23:02:45 +0530

    update

--- index.js
+++ index.js
@@ -22,20 +22,44 @@ document.querySelector(".form-heading-getStarted").addEventListener("click",func
 
 var btn_moon = document.querySelector(".Themoon");
 var btn_mars = document.querySelector(".mars");
+var btn_jupiter = document.querySelector(".jupiter");
+var btn_saturn = document.querySelector(".saturn");
+
+
 
 var the_moon = document.querySelector(".tabs-content-moon");
 var mars = document.querySelector(".tabs-content-mars");
+var jupiter = document.querySelector(".tabs-content-jupiter");
+var saturn = document.querySelector(".tabs-content-saturn");
 
 
 
 btn_moon.addEventListener("click",()=>{
     the_moon.style.display="block";
     mars.style.display="none";
-    
+    jupiter.style.display="none";
+    saturn.style.display="none";
+
 })
 
 
 btn_mars.addEventListener("click",()=>{
     the_moon.style.display="none";
     mars.style.display="block";
+    jupiter.style.display="none";
+    saturn.style.display="none";
+})
+
+
+btn_jupiter.addEventListener("click",()=>{
+    the_moon.style.display="none";
+    mars.style.display="none";
+    jupiter.style.display="block";
+    saturn.style.display="none";
+})
+btn_saturn.addEventListener("click",()=>{
+    the_moon.style.display="none";
+    mars.style.display="none";
+    jupiter.style.display="none";
+    saturn.style.display="block";
 })
