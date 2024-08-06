1ef1837149e6f2bb75b5c277c0497dd6790630c2
Ajay Yadav <97391719+Ajayraoyadav@users.noreply.github.com>
2024-08-06 22:24:49 +0530

    update

--- index.js
+++ index.js
@@ -17,3 +17,25 @@ document.getElementById('increase').addEventListener('click', function() {
 document.querySelector(".form-heading-getStarted").addEventListener("click",function(){
     alert("Hlo Moon");
 })
+
+
+
+var btn_moon = document.querySelector(".Themoon");
+var btn_mars = document.querySelector(".mars");
+
+var the_moon = document.querySelector(".tabs-content-moon");
+var mars = document.querySelector(".tabs-content-mars");
+
+
+
+btn_moon.addEventListener("click",()=>{
+    the_moon.style.display="block";
+    mars.style.display="none";
+    
+})
+
+
+btn_mars.addEventListener("click",()=>{
+    the_moon.style.display="none";
+    mars.style.display="block";
+})
